71f76ca882ba38bde97b4771ef7b802f4eaf07b2
Rodrigo Ramos de Freitas <rodrigoramos2002@outlook.com>
2023-09-26 07:54:01 -0300

    Adiconando testes com utilizacao de exececao

--- src/test/java/tdd/service/BonusServiceTest.java
+++ src/test/java/tdd/service/BonusServiceTest.java
@@ -9,35 +9,40 @@ import org.junit.jupiter.api.Test;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class BonusServiceTest {
 
 	@Test
 	public void bonusDeveriaSerZeroParaFuncionarioComSalarioMuitoAlto() {
 		BonusService service = new BonusService();
-		BigDecimal bonus = service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("25000")));
+		//assertThrows(IllegalArgumentException.class,
+		// () -> service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("25000"))));
 
-		assertEquals(new BigDecimal("00.00"), bonus);
-
+		try {
+			service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("25000")));
+			fail("Não deu a exeception!");
+		} catch (Exception e) {
+			assertEquals("Funcionario com salario maior do que R$10000 nao pode receber bonus!", e.getMessage());
+		}
 	}
 
-	@Test
-	public void bonusDeveriaSerDezPorCentoDoSalario() {
-		BonusService service = new BonusService();
-		BigDecimal bonus = service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("2500")));
+		@Test
+		public void bonusDeveriaSerDezPorCentoDoSalario() {
+			BonusService service = new BonusService();
+			BigDecimal bonus = service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("2500")));
 
-		assertEquals(new BigDecimal("250.00"), bonus);
+			assertEquals(new BigDecimal("250.00"), bonus);
 
-	}
+		}
 
-	@Test
-	public void bonusDeveriaSerDezPorcentoParaSalarioDe10000() {
-		BonusService service = new BonusService();
-		BigDecimal bonus = service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("10000")));
+		@Test
+		public void bonusDeveriaSerDezPorcentoParaSalarioDe10000() {
+			BonusService service = new BonusService();
+			BigDecimal bonus = service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("10000")));
 
-		assertEquals(new BigDecimal("1000.00"), bonus);
+			assertEquals(new BigDecimal("1000.00"), bonus);
 
-	}
+		}
 
 }
